2c3d8dd13550c34dfe0c92f1174800afe6b9df2d
Ivan Mudarth <ivan.mudarth@yahoo.com>
2020-07-03 01:51:41 -0400

    client test

--- Queues/Permutation.java
+++ Queues/Permutation.java
@@ -1,7 +1,24 @@
 package Queues;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        int k = StdIn.readInt();
+        String s[] = StdIn.readAllStrings();
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        int count = 0;
 
+        for (int i = 0; i < s.length; i++) { // read each item in sequence of strings
+            queue.enqueue(s[i]);
+        }
+        for (String a : queue) { // print k items, uniformly, at random
+            count++;
+            if (count == k)
+                break;
+            else
+                StdOut.println(a);
+        }
     }
 }
